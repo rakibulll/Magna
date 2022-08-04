32ea15a06847a3708d7c670e4a9c719dfce5faf6
Rakibul Hassan <59486222+rakibulll@users.noreply.github.com>
2022-08-04 11:20:43 -0400

    update temperature script

--- script.py
+++ script.py
@@ -2,4 +2,4 @@ import os
 from time import time, sleep
 while True:
     sleep(10 - time() % 10)
-    os.system("paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'")
+    os.system("cat /sys/class/thermal/thermal_zone0/temp | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'")
