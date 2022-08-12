9dd2b21cbe2bcccfa16bd6151ad6e86eecf06d56
Rakibul Hassan <59486222+rakibulll@users.noreply.github.com>
2022-08-12 12:31:13 -0400

    Update SPI_test.py

--- SPI_test.py
+++ SPI_test.py
@@ -2,6 +2,7 @@ from pyftdi.spi import SpiController, SpiIOError
 from time import time, sleep
 
 spi = SpiController(cs_count=2)
+#URL scheme to follow: ftdi://[vendor][:[product][:serial|:bus:address|:index]]/interface
 spi.configure('ftdi://ftdi:232h:1/1')
 slave = spi.get_port(cs=0, freq=10E6, mode=2)
 
