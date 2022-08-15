f7309dce969efc0702d45ece45b1211493d6ff85
Rakibul Hassan <59486222+rakibulll@users.noreply.github.com>
2022-08-15 11:47:17 -0400

    beaglebone AI-64 video test

---
+++ video.py
@@ -0,0 +1,17 @@
+import cv2
+
+cv2.namedWindow("output")
+cap = cv2.VideoCapture(0)
+
+if cap.isOpened():              # Getting the first frame
+    ret, frame = cap.read()
+else:
+    ret = False
+
+while ret:
+    cv2.imshow("output", frame)
+    ret, frame = cap.read()
+    key = cv2.waitKey(20)
+    if key == 27:                    # exit on Escape key
+        break
+cv2.destroyWindow("output")
